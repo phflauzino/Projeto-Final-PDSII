3ab3015898f7c0cfa3c83ee4661073256cec3afa
viniciusmsil <viniciusmoraissilva@ufmg.br>
2025-01-29 20:44:47 -0300

    Update lig4.hpp

--- src/games/lig4.hpp
+++ src/games/lig4.hpp
@@ -2,20 +2,21 @@
 #define LIG4_HPP
 
 #include "tabuleiro.hpp"
-#include "jogador.hpp"
+#include "../players/jogador.hpp"
+#include <memory> 
 
 class Lig4 : public Tabuleiro {
 private:
-    Jogador jogador1, jogador2;
-    Jogador* jogadorAtual;
+    std::shared_ptr<Jogador> jogador1, jogador2;  
+    std::shared_ptr<Jogador> jogadorAtual; 
 
 public:
-    Lig4(const Jogador& jogador1, const Jogador& jogador2);
+    Lig4(const std::shared_ptr<Jogador>& jogador1, const std::shared_ptr<Jogador>& jogador2);
 
     void jogar();
     bool inserirPeca(int coluna, char simbolo);
     bool verificarVitoria(char simbolo) const;
-    void verificarVencedor() const; 
+    void verificarVencedor() const;
 };
 
 #endif
