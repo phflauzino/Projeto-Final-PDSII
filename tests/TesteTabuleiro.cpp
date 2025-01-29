1ce53333ddbf2e262fba413b8d495a8edd4e60cc
Matheus845 <matheus.guedes.belloni1973@gmail.com>
2025-01-28 23:52:11 -0300

    Update TesteTabuleiro.cpp

--- tests/TesteTabuleiro.cpp
+++ tests/TesteTabuleiro.cpp
@@ -1,22 +1,44 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
 #include "tabuleiro.hpp"
+#include "doctest.h"
 
-TEST_CASE("Testando construtores do Tabuleiro") {
-  Tabuleiro tab1;
-  CHECK(tab1.getTamanho() == 3);
+TEST_CASE("Testando o Tabuleiro") {
+    // Teste de criação do tabuleiro quadrado
+    Tabuleiro tab1(3);  // Tabuleiro 3x3
+    CHECK(tab1.getPosicao(0, 0) == ' ');
+    CHECK(tab1.getPosicao(1, 1) == ' ');
+    CHECK(tab1.getPosicao(2, 2) == ' ');
 
-  Tabuleiro tab2(5);
-  CHECK(tab2.getTamanho() == 5);
-}
-
-TEST_CASE("Testando limpeza do tabuleiro") {
-  Tabuleiro tab(4);
-  tab.limparTabuleiro();
-
-  for (int i = 0; i < tab.getTamanho(); ++i) {
-    for (int j = 0; j < tab.getTamanho(); ++j) {
-      CHECK(tab.getCelula(i, j) == ' ');
+    SUBCASE("Testando criação de tabuleiro retangular") {
+        Tabuleiro tab2(3, 5);  // Tabuleiro 3x5
+        CHECK(tab2.getPosicao(0, 0) == ' ');
+        CHECK(tab2.getPosicao(2, 4) == ' ');
+        CHECK(tab2.getPosicao(1, 2) == ' ');
     }
-  }
+
+    SUBCASE("Testando o método estaVazio") {
+        // Criando um tabuleiro 3x3 e verificando se está vazio
+        Tabuleiro tab3(3);
+        CHECK(tab3.estaVazio(0, 0) == true);
+        tab3.setPosicao(0, 0, 'X');
+        CHECK(tab3.estaVazio(0, 0) == false);  // Agora a posição está ocupada
+    }
+
+    SUBCASE("Testando o método setPosicao e getPosicao") {
+        Tabuleiro tab4(3);
+        tab4.setPosicao(1, 1, 'O');
+        CHECK(tab4.getPosicao(1, 1) == 'O');
+        tab4.setPosicao(1, 1, 'X');
+        CHECK(tab4.getPosicao(1, 1) == 'X');
+    }
+
+    SUBCASE("Testando a exibição do tabuleiro") {
+        Tabuleiro tab5(3);
+        tab5.setPosicao(0, 0, 'X');
+        tab5.setPosicao(1, 1, 'O');
+        tab5.setPosicao(2, 2, 'X');
+        
+        // Espera-se que o tabuleiro seja impresso corretamente
+        tab5.exibirTabuleiro();
+        // Aqui você pode manualmente verificar a saída no console ou usar outra estratégia de captura de saída
+    }
 }
