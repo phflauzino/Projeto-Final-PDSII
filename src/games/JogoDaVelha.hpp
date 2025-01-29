46f3db5aa19f72ad0c628fc49fbdd321acaa2606
viniciusmsil <viniciusmoraissilva@ufmg.br>
2025-01-29 20:49:40 -0300

    Update JogoDaVelha.hpp

--- src/games/JogoDaVelha.hpp
+++ src/games/JogoDaVelha.hpp
@@ -2,6 +2,8 @@
 #define JOGODAVELHA_HPP
 
 #include "tabuleiro.hpp"
+#include "../players/jogador.hpp"
+#include <memory>
 
 class JogoDaVelha : public Tabuleiro {
 private:
@@ -14,6 +16,7 @@ public:
     bool verificarVitoria(char jogador) const;
     bool verificarEmpate() const;
     void passarTurno();
+    void jogar();
     int getTurnoAtual() const { return turnoAtual; }
 };
 
