9c5678095299c217bee8a5817d3843c5b01b9ca4
Gledson Paiva <gleds.hpaiva2@gmail.com>
2025-01-29 21:53:26 -0300

    Update othello.hpp

--- src/games/othello.hpp
+++ src/games/othello.hpp
@@ -1,39 +1,28 @@
 #ifndef OTHELLO_HPP
 #define OTHELLO_HPP
 
-#include "../players/jogador.hpp"
-#include <memory>
 #include <vector>
+#include <memory>
+#include "jogador.hpp"
 
 class Othello {
-private:
-    std::shared_ptr<Jogador> jogador1, jogador2;  
-    bool turnoJogador1;  
-    std::vector<std::vector<char>> tabuleiro;  
-
- 
-    void inicializarTabuleiro();
-
-    bool jogadaValida(int linha, int coluna);
-
-
-    void capturarPecas(int linha, int coluna);
-
 public:
-
     Othello(std::shared_ptr<Jogador> j1, std::shared_ptr<Jogador> j2);
-
 
     void jogar();
 
-
+private:
+    void inicializarTabuleiro();
+    bool jogadaValida(int linha, int coluna);
+    void capturarPecas(int linha, int coluna);
     bool verificarFimDeJogo();
-
-
     void exibirJogadorAtual();
-
-
     void exibirTabuleiro();
+
+    std::shared_ptr<Jogador> jogador1;
+    std::shared_ptr<Jogador> jogador2;
+    bool turnoJogador1;
+    std::vector<std::vector<char>> tabuleiro;
 };
 
-#endif 
+#endif
